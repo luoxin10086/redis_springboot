44d83cefe7d980d1f8cae0911c48682cf9d06a52
luoxin <15928438186@163.com>
2022-09-07 15:25:29 +0800

    123

--- src/main/java/com/atguigu/redis_springboot/config/GitTest.java
+++ src/main/java/com/atguigu/redis_springboot/config/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.redis_springboot.config;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("test1");
+        System.out.println("test2");
     }
 }
