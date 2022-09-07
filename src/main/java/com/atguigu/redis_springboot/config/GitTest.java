719b4672105e79aa302a79193b30bb3bb644c35a
luoxin <15928438186@163.com>
2022-09-07 15:52:09 +0800

    123

--- src/main/java/com/atguigu/redis_springboot/config/GitTest.java
+++ src/main/java/com/atguigu/redis_springboot/config/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("master2");
         System.out.println("hot-fi2");
+        System.out.println("push test!");
     }
 }
